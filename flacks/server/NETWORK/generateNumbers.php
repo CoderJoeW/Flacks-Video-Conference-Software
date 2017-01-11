d37c46a7fc97cd9fffb06bb80943969a696dbccd
flaminggenius <livewiretech.reply.mail@gmail.com>
2017-01-11 03:55:21 -0500

    Update generateNumbers.php
    nicer algorithm

--- flacks/server/NETWORK/generateNumbers.php
+++ flacks/server/NETWORK/generateNumbers.php
@@ -4,7 +4,7 @@
 	$number = GenerateNumber();
     
     $sql = "SELECT * FROM numbers WHERE number='$number'";
-    $query = mysql_query($sql);
+    $query = mysql_query($sql) or die(mysql_error());
     $row = mysql_fetch_object($query);
     
     $id = $row->id;
@@ -17,18 +17,7 @@
     }
     
     function GenerateNumber(){
-    	$a1 = rand(1,9);
-    	$a2 = rand(1,9);
-    	$a3 = rand(1,9);
-    	$f1 = rand(1,9);
-    	$f2 = rand(1,9);
-    	$f3 = rand(1,9);
-    	$l1 = rand(1,9);
-    	$l2 = rand(1,9);
-    	$l3 = rand(1,9);
-    	$l4 = rand(1,9);
-        
-        $num = $a1.$a2.$a3.$f1.$f2.$f3.$l1.$l2.$l3.$l4;
+        $num = rand(1111111111,9999999999);
         
         return $num;
     }
