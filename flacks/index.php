6ece80b3f3515d0a956f91c2d08d37ede79daf8c
flaminggenius <livewiretech.reply.mail@gmail.com>
2017-04-18 17:17:54 -0400

    Update index.php

--- flacks/index.php
+++ flacks/index.php
@@ -1,6 +1,24 @@
+<? require_once("sever/connect.php"); ?>
 <!DOCTYPE html>
-<html>
-<?php include('flacks/overall/overall_header.php'); ?>
+<html lang="en">
+<head>
+<title>Flacks | Video Conference Software</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
+
+<link rel="stylesheet" href="css/main.css?v=0.0.1">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+<!-- Login Stylesheet //Michael Mavashev -->
+<link rel="stylesheet" href="flacks/login/css/styles.css">
+
+<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+</head>
 <body>
 <script src="js/top.js?v=0.0.1"></script>
 <div class="container">
